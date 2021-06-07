ffaa6eacbf4ef39405f66ee8c16567ad8710915c
Mob-Anish <mobanish16@gmail.com>
2021-06-07 13:41:37 +0545

    Admin dashboard ui design completed

--- app.js
+++ app.js
@@ -40,17 +40,52 @@ document.querySelector(".btn--new").addEventListener("click", (e) => {
   document.querySelector(".new--product_container").style.display = "block";
 });
 
-document.querySelector(".user-section .user--new .btn--new").addEventListener("click", (e) => {
-  document.querySelector(".user-section").style.display = "none";
-  document.querySelector(".new--user_container").style.display = "block";
-});
+document
+  .querySelector(".product-section .product-container .values a")
+  .addEventListener("click", (e) => {
+    document.querySelector(".product-section").style.display = "none";
+    document.querySelector(".update--product_container").style.display =
+      "block";
+  });
 
-document.querySelector(".new--product_header .back--btn").addEventListener('click', e => {
-  document.querySelector('.product-section').style.display = "block";
-  document.querySelector('.new--product_container').style.display = "none";
-});
+document
+  .querySelector(".user-section .user--new .btn--new")
+  .addEventListener("click", (e) => {
+    document.querySelector(".user-section").style.display = "none";
+    document.querySelector(".new--user_container").style.display = "block";
+  });
 
-document.querySelector(".new--user_header .back--btn").addEventListener('click', e => {
-  document.querySelector('.user-section').style.display = "block";
-  document.querySelector('.new--user_container').style.display = "none";
-});
+document
+  .querySelector(".user-section .user-container .values a")
+  .addEventListener("click", (e) => {
+    document.querySelector(".user-section").style.display = "none";
+    document.querySelector(".update--user_container").style.display = "block";
+  });
+
+document
+  .querySelector(".new--product_header .back--btn")
+  .addEventListener("click", (e) => {
+    document.querySelector(".product-section").style.display = "block";
+    document.querySelector(".new--product_container").style.display = "none";
+  });
+
+document
+  .querySelector(".update--product_header .back--btn")
+  .addEventListener("click", (e) => {
+    document.querySelector(".product-section").style.display = "block";
+    document.querySelector(".update--product_container").style.display = "none";
+  });
+
+document
+  .querySelector(".new--user_header .back--btn")
+  .addEventListener("click", (e) => {
+    document.querySelector(".user-section").style.display = "block";
+    document.querySelector(".new--user_container").style.display = "none";
+  });
+
+document
+  .querySelector(".update--user_header .back--btn")
+  .addEventListener("click", (e) => {
+    document.querySelector(".user-section").style.display = "block";
+    document.querySelector(".update--user_container").style.display = "none";
+  });
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 ## Admin Dashboard
 
 ![Screenshot (371)](https://user-images.githubusercontent.com/75711381/119992448-47a23f80-bfea-11eb-8ed3-9ab9c72042d9.png)
+
+#### The above admin dashboard is used in my e-commerce web application. You can see my github repo 👉👉 [ShopMe.](https://github.com/Mob-Anish/ShopMe)
